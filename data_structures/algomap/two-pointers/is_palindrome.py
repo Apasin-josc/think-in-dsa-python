89616633b36bd640affe5b41d76239954bfd875d
José Omar <82555012+Apasin-josc@users.noreply.github.com>
2026-02-09 09:55:09 +0100

    Add palindrome checking function in is_palindrome.py
    Implement a function to check if a string is a palindrome by ignoring non-alphanumeric characters and case.

---
+++ data_structures/algomap/two-pointers/is_palindrome.py
@@ -0,0 +1,47 @@
+""" 
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing 
+all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+ 
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome. 
+"""
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        while left <= right:
+            if not s[left].isalnum():       #alnum returns true if all the characters are alphanumeric (a-z / 0-9) | false (space)!@#$% etc
+                left += 1
+                continue
+            
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
+    
+sol = Solution()
+print(sol.isPalindrome("A man, a plan, a canal: Panama"))
+print(sol.isPalindrome("race a car"))
+print(sol.isPalindrome(" "))
