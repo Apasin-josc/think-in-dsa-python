cff4c61c98a76f2fcb7ddb3e863d9b76ba00729c
José Omar <82555012+Apasin-josc@users.noreply.github.com>
2026-03-30 12:26:36 +0200

    Add validWordSquare function to validate word squares
    Implement a function to check if an array of strings forms a valid word square by comparing rows and columns.

---
+++ data_structures/neetcode/arrays_and_hashing/valid_words_square.py
@@ -0,0 +1,79 @@
+""" Given an array of strings words, return true if it forms a valid word square.
+
+A sequence of strings forms a valid word square if the kth row and column read
+the same string, where 0 <= k < max(numRows, numColumns).
+
+Example 1:
+Input: words = ["abcd",
+                "bnrt",
+                "crmy",
+                "dtye"]
+x
+Output: true
+Explanation:
+The 1st row and 1st column both read "abcd".
+The 2nd row and 2nd column both read "bnrt".
+The 3rd row and 3rd column both read "crmy".
+The 4th row and 4th column both read "dtye".
+Therefore, it is a valid word square.
+
+Example 2:
+Input: words = ["abcd",
+                "bnrt",
+                "crm",
+                "dt"]
+
+Output: true
+Explanation:
+The 1st row and 1st column both read "abcd".
+The 2nd row and 2nd column both read "bnrt".
+The 3rd row and 3rd column both read "crm".
+The 4th row and 4th column both read "dt".
+Therefore, it is a valid word square.
+
+Example 3:
+Input: words = ["ball",
+                "area",
+                "read",
+                "lady"]
+
+Output: false
+Explanation:
+The 3rd row reads "read" while the 3rd column reads "lead".
+Therefore, it is NOT a valid word square. 
+"""
+from typing import List
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+        for i in range(len(words)):
+            for j in range(len(words)):
+                
+                if i >= len(words[j]) and j >= len(words[i]):
+                    continue
+                
+                if i >= len(words[j]) or j >= len(words[i]):
+                    return False
+                
+                if words[i][j] != words[j][i]:
+                    return False
+        
+        return True
+
+sol = Solution()
+print(sol.validWordSquare(["abcd",
+                            "bnrt",
+                            "crmy",
+                            "dtye"]))
+
+print(sol.validWordSquare(["abcd",
+                            "bnrt",
+                            "crm",
+                            "dt"]))
+
+print(sol.validWordSquare(["ball",
+                            "area",
+                            "read",
+                            "lady"]))
+
+#T: O(n^2)
+#S: O(1)
